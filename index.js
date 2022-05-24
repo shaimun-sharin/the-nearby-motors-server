08425b6c0be56ce3010a03b9d9b430968870f8fe
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-24 15:13:32 +0600

    DB connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -14,3 +15,25 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Nearby Motors listening on port ${port}`);
 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@thenearbymotors.bwksm45.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("nearby-motors").collection("products");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
