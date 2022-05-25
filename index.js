9b2af14db17de9a3f127ac1bdd47d545091f7856
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-25 20:33:43 +0600

    Admin Api created

--- index.js
+++ index.js
@@ -89,9 +89,20 @@ async function run() {
       );
       res.send({ result, token });
     });
-    app.get("/user", async (req, res) => {
+    app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
+    });
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      const filter = { email: email };
+
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     // app.put("/product/:id", async (req, res) => {
